5b2a2d775fa4322e066897b899b1cabf4126b975
M-Marlewski <mmarlewski1234@gmail.com>
2022-05-22 22:27:57 +0200

    New isometric Pan Klex

--- core/src/com/marcin/panklex/Enums.kt
+++ core/src/com/marcin/panklex/Enums.kt
@@ -1,21 +1,6 @@
 package com.marcin.panklex
 
-enum class Direction
-{
-    None, Up, Down, Left, Right
-}
-
 enum class Block
 {
     Empty, UndamagedStone, DamagedStone, Brick, Metal
-}
-
-enum class Action
-{
-    None, Pickaxe, Bomb, Coin, Cell, Hand, Walk
-}
-
-enum class MapLayer
-{
-    Pit, Hole, Base, Block, Wall, Entity, Cover, Player
 }
--- core/src/com/marcin/panklex/PanKlexGame.kt
+++ core/src/com/marcin/panklex/PanKlexGame.kt
@@ -13,12 +13,7 @@ class PanKlexGame : Game()
     lateinit var batch : SpriteBatch
     val assetManager = AssetManager()
 
-    lateinit var screenMainMenu : ScreenMainMenu
     lateinit var screenGame : ScreenGame
-    lateinit var containerScreen : ContainerScreen
-    lateinit var vendingMachineScreen : VendingMachineScreen
-    lateinit var elevatorScreen : ElevatorScreen
-    lateinit var screenEndGame : ScreenEndGame
 
     fun exit()
     {
@@ -42,31 +37,7 @@ class PanKlexGame : Game()
 
     fun loadAssets()
     {
-        assetManager.load("graphics/hud/heart.png", Texture::class.java)
-
-        assetManager.load("graphics/actions/pickaxe.png", Texture::class.java)
-        assetManager.load("graphics/actions/bomb.png", Texture::class.java)
-        assetManager.load("graphics/actions/coin.png", Texture::class.java)
-        assetManager.load("graphics/actions/cell.png", Texture::class.java)
-        assetManager.load("graphics/actions/hand.png", Texture::class.java)
-        assetManager.load("graphics/actions/walk.png", Texture::class.java)
-        assetManager.load("graphics/actions/cancel.png", Texture::class.java)
-
-        assetManager.load("graphics/entities/player.png", Texture::class.java)
-        assetManager.load("graphics/entities/container.png", Texture::class.java)
-        assetManager.load("graphics/entities/vendingMachine.png", Texture::class.java)
-        assetManager.load("graphics/entities/teleporter.png", Texture::class.java)
-        assetManager.load("graphics/entities/elevator.png", Texture::class.java)
-        assetManager.load("graphics/entities/flag.png", Texture::class.java)
-        assetManager.load("graphics/entities/stairs.png", Texture::class.java)
-        assetManager.load("graphics/entities/poweredGate.png", Texture::class.java)
-
-        assetManager.load("graphics/blocks/special.png", Texture::class.java)
-        assetManager.load("graphics/blocks/example.png", Texture::class.java)
-        assetManager.load("graphics/blocks/undamagedStone.png", Texture::class.java)
-        assetManager.load("graphics/blocks/damagedStone.png", Texture::class.java)
-        assetManager.load("graphics/blocks/brick.png", Texture::class.java)
-        assetManager.load("graphics/blocks/metal.png", Texture::class.java)
+        //assetManager.load("graphics/hud/heart.png", Texture::class.java)
 
         assetManager.finishLoading()
     }
@@ -75,24 +46,14 @@ class PanKlexGame : Game()
     {
         loadAssets()
         batch = SpriteBatch()
-        screenMainMenu = ScreenMainMenu("screen MainMenu", this)
         screenGame = ScreenGame("screen Game", this)
-        containerScreen = ContainerScreen("screen Container", this)
-        vendingMachineScreen = VendingMachineScreen("screen VendingMachine", this)
-        elevatorScreen = ElevatorScreen("screen Elevator", this)
-        screenEndGame = ScreenEndGame("screen EndGame", this)
-        changeScreen(screenMainMenu)
+        changeScreen(screenGame)
     }
 
     override fun dispose()
     {
         batch.dispose()
         assetManager.dispose()
-        screenMainMenu.dispose()
         screenGame.dispose()
-        containerScreen.dispose()
-        vendingMachineScreen.dispose()
-        elevatorScreen.dispose()
-        screenEndGame.dispose()
     }
 }
